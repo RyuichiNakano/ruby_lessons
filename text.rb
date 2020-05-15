f1ae5e834af813525e150343a1fb295bccfd1c56
EC2 Default User <ec2-user@ip-172-31-81-106.ec2.internal>
2020-05-15 15:06:21 +0000

    コードを考えてみた

--- text.rb
+++ text.rb
@@ -3,3 +3,18 @@ puts 'こんにちは'
 =begin
 わいわいはなしたい
 =end
+
+class User #クラスの型
+  def name #name メソッド
+    @name #インスタンス変数
+  end
+ 
+  def name=(name) #メソッドに引数を渡す（渡すで考え方が正しいか確認）
+    @name = name　#sefl.name に nameを代入
+  end
+ 
+end
+ 
+user = User.new　#Userクラスにinitialive newで新しい型を代入
+user.name = "taro" # userクラスの中のnameに "taro"を代入
+p user.name #p(puts?) user.name で　"taro" を出力
